b53a61eec90fee78e7448d3341353e5aba61b92c
S Sathvik <154729057+SathvikSimha@users.noreply.github.com>
2026-01-12 22:12:23 +0530

    nothing

--- simulation/prog2.tcl
+++ simulation/prog2.tcl
@@ -1,52 +1,42 @@
-#Create Simulator
 set ns [new Simulator]
 
-#Use colors to differentiate the traffic
-$ns color 1 Blue
-$ns color 2 Red
+# Colors
+$ns color 1 Blue      ;# Ping
+$ns color 2 Red       ;# TCP Congestion
 
-#Open trace and NAM trace file
-set ntrace [open prog2.tr w]
-$ns trace-all $ntrace
-set namfile [open prog2.nam w]
-$ns namtrace-all $namfile
+# Trace & NAM files
+set tf [open out.tr w]
+$ns trace-all $tf
+set nf [open out.nam w]
+$ns namtrace-all $nf
 
-#Finish Procedure
-proc Finish {} {
-global ns ntrace namfile
+proc finish {} {
+    global ns tf nf
+    $ns flush-trace
+    close $tf
+    close $nf
+    exec nam out.nam &
 
-#Dump all trace data and close the file
-$ns flush-trace
-close $ntrace
-close $namfile
+    puts "Number of ping packets dropped:"
+    exec grep "^d" out.tr | grep ping | wc -l
 
-#Execute the nam animation file
-exec nam prog2.nam &
-
-#Find the number of ping packets dropped
-puts "The number of ping packets dropped are "
-exec grep "^d" prog2.tr | cut -d " " -f 5 | grep -c "ping" &
-exit 0
+    exit 0
 }
 
-#Create six nodes
+# Create 6 nodes
 for {set i 0} {$i < 6} {incr i} {
-set n($i) [$ns node]
+    set n($i) [$ns node]
 }
 
-#Connect the nodes
-for {set j 0} {$j < 5} {incr j} {
-$ns duplex-link $n($j) $n([expr ($j+1)]) 0.1Mb 10ms DropTail
+# Connect nodes
+for {set i 0} {$i < 5} {incr i} {
+    $ns duplex-link $n($i) $n([expr $i+1]) 0.1Mb 10ms DropTail
 }
 
-#Define the recv function for the class 'Agent/Ping'
-Agent/Ping instproc recv {from rtt} {
-$self instvar node_
-puts "node [$node_ id] received ping answer from $from with round trip time $rtt
-ms"
-}
+# Force congestion
+$ns queue-limit $n(2) $n(3) 2
 
-#Create two ping agents and attach them to n(0) and n(5)
+# Ping agents (Blue)
 set p0 [new Agent/Ping]
 $p0 set class_ 1
 $ns attach-agent $n(0) $p0
@@ -55,36 +45,28 @@ $p1 set class_ 1
 $ns attach-agent $n(5) $p1
 $ns connect $p0 $p1
 
-#Set queue size and monitor the queue
-#Queue size is set to 2 to observe the drop in ping packets
-$ns queue-limit $n(2) $n(3) 2
-$ns duplex-link-op $n(2) $n(3) queuePos 0.5
+# TCP congestion traffic (Red)
+set tcp [new Agent/TCP]
+$tcp set class_ 2
+$ns attach-agent $n(2) $tcp
+set sink [new Agent/TCPSink]
+$ns attach-agent $n(4) $sink
+$ns connect $tcp $sink
 
-#Create Congestion
-#Generate a Huge CBR traffic between n(2) and n(4)
-set tcp0 [new Agent/TCP]
-$tcp0 set class_ 2
-$ns attach-agent $n(2) $tcp0
-set sink0 [new Agent/TCPSink]
-$ns attach-agent $n(4) $sink0
-$ns connect $tcp0 $sink0
+set cbr [new Application/Traffic/CBR]
+$cbr set packetSize_ 500
+$cbr set rate_ 1Mb
+$cbr attach-agent $tcp
 
-#Apply CBR traffic over TCP
-set cbr0 [new Application/Traffic/CBR]
-$cbr0 set packetSize_ 500
-$cbr0 set rate_ 1Mb
-$cbr0 attach-agent $tcp0
-
-#Schedule events
+# Schedule
 $ns at 0.2 "$p0 send"
 $ns at 0.4 "$p1 send"
-$ns at 0.4 "$cbr0 start"
+$ns at 0.4 "$cbr start"
 $ns at 0.8 "$p0 send"
 $ns at 1.0 "$p1 send"
-$ns at 1.2 "$cbr0 stop"
+$ns at 1.2 "$cbr stop"
 $ns at 1.4 "$p0 send"
 $ns at 1.6 "$p1 send"
-$ns at 1.8 "Finish"
+$ns at 1.8 "finish"
 
-#Run the Simulation
 $ns run
